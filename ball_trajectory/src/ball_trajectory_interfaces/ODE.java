85e52aa36641e4ab38807721c7d4d32d05510139
HenriqueMuniz <phmb3@cin.ufpe.br>
2018-03-09 21:37:05 -0300

    Projeto Up

---
+++ ball_trajectory/src/ball_trajectory_interfaces/ODE.java
@@ -0,0 +1,9 @@
+package ball_trajectory_interfaces;
+
+public interface ODE {
+	public double[] getState();
+	public static void getRate(double[] state, double[] rate) {
+		// TODO Auto-generated method stub
+		
+	}
+}
